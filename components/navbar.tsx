29c66be4b30b20c67264b75182daffa42ed18472
MatthewMuir-MLI <106112115+MatthewMuir-MLI@users.noreply.github.com>
2026-02-10 18:23:32 -0800

    updated nav bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -68,24 +68,6 @@ export default function Navbar() {
                 </a>
               </li>
 
-              <li className="bg-transparent">
-                <a
-                  className="text-xl hover:text-primary hover:bg-transparent font-[Changa_One] tracking-widest"
-                  href="/demo_rebus"
-                >
-                  Rebus Puzzles
-                </a>
-              </li>
-
-              <li className="bg-transparent">
-                <a
-                  className="text-xl hover:text-primary hover:bg-transparent font-[Changa_One] tracking-widest"
-                  href="/trivia"
-                >
-                  Trivia Questions
-                </a>
-              </li>
-
               {/* Version text at bottom */}
               <div className="absolute bottom-4 left-0 right-0 flex justify-center">
                 <span className="text-xs text-base-content/50">
